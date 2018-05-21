1149967462aa72cfbae705a614a1cb1ff4674df5
Keith O'Brien <keith@keithobrien.org>
2018-05-20 21:29:52 -0400

    New Script

--- domain_parser.py
+++ domain_parser.py
@@ -4,21 +4,26 @@ import re
 
 #parses from malwaredomains.com
 
-resp = requests.get("http://malc0de.com/bl/ZONES")
-txt = resp.text
+urls = ['http://cybercrime-tracker.net/all.php', 'http://mirror1.malwaredomains.com/files/domains.txt', 'http://malc0de.com/bl/ZONES']
+output = ['cybercrimetracker.txt', 'malwaredomains.txt', 'malcode.txt']
 
-lines = txt.splitlines()
 
-domains = []
-for line in lines:
-    match = re.search(r'\b((xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b', line)
-    if match:
-        domains.append(match.group())
+for index, url in enumerate(urls, start=0):
+    print('Getting {t}'.format(t=url))
+    resp = requests.get(url)
+    txt = resp.text
+    lines = txt.splitlines()
+    domains = []
+    for line in lines:
+        match = re.search(r'\b((xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b', line)
+        if match:
+            domains.append(match.group())
 
-fn = "malcode.txt"
-f = open(fn, 'w')
-for line in domains:
-    if 'malc0de.com' not in line and 'www.malwaredomains.com' not in line:
-        print(line, file=f)
-f.close()
+    fn = output[index]
+    f = open(fn, 'w')
+    print('Writing {l} \n'.format(l=fn))
+    for line in domains:
+        if 'malc0de.com' not in line and 'www.malwaredomains.com' not in line:
+            print(line, file=f)
+    f.close()
 
