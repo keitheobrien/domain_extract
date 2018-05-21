caae0aa63fc97455816a8d9b6ce5fbe8e70b7c29
Keith O'Brien <keith@keithobrien.org>
2018-05-20 21:36:13 -0400

    New Script

--- domain_parser.py
+++ domain_parser.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import requests
 import re
-
-#parses from malwaredomains.com
+# urls list containts the urls of the threat intel site that list the malicous domains
+# output list is the files that should be written to for each threat intel site
 
 urls = ['http://cybercrime-tracker.net/all.php', 'http://mirror1.malwaredomains.com/files/domains.txt', 'http://malc0de.com/bl/ZONES']
 output = ['cybercrimetracker.txt', 'malwaredomains.txt', 'malcode.txt']
-
 
 for index, url in enumerate(urls, start=0):
     print('Getting {t}'.format(t=url))
@@ -18,7 +17,6 @@ for index, url in enumerate(urls, start=0):
         match = re.search(r'\b((xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b', line)
         if match:
             domains.append(match.group())
-
     fn = output[index]
     f = open(fn, 'w')
     print('Writing {l} \n'.format(l=fn))
